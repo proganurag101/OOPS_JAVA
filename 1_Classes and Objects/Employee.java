119cc5ec1db077e0085ce4463a44c5d6dc3f8ac1
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-01-23 15:34:26 +0530

    Update Employee.java

--- 1_Classes and Objects/Employee.java
+++ 1_Classes and Objects/Employee.java
@@ -2,7 +2,7 @@ public class Employee{
     String empName;
     int empId;
     int empSal;
-    public void disBonus(float salary){
+    public void disBonus(float salary){     //This function can be called from inside as well as outside from another class.
         System.out.println("Bonus: "+salary*0.20);
 
     }
