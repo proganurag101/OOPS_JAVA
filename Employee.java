4e8e517b782996aa72c393cbfe45907b7d8c54ee
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-01-23 15:03:59 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+public class Employee{
+    String empName;
+    int empId;
+    int empSal;
+    public void disBonus(float salary){
+        System.out.println("Bonus: "+salary*0.20);
+
+    }
+
+
+
+
+}
