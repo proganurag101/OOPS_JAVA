aafa250da68b3c57a85ee06bd778a37d34affd66
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-01-31 16:59:54 +0530

    Create main.java

---
+++ Inheritance/Single Inheritance/main.java
@@ -0,0 +1,15 @@
+
+class Proj{
+
+
+
+    public static void main(String[] args) {
+        Class2 res = new Class2();
+        res.num1 = 100;
+        res.num2 = 50;
+        res.doAddition();
+        res.doSubtraction();
+
+
+    }
+}
