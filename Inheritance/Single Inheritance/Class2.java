d2e78c6ff8a78f0c4369df20616a0f5f54d6e2df
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-01-31 17:00:45 +0530

    Create Class2.java

---
+++ Inheritance/Single Inheritance/Class2.java
@@ -0,0 +1,6 @@
+public class Class2 extends Class1{ //class 2 inherits from class1
+    public void doSubtraction(){
+        System.out.println("Subtraction is "+(num1-num2)); //num1,2 are members of class1
+    }
+
+}
