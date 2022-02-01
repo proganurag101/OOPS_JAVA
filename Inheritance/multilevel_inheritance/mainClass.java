b9f05e75b87bc4a20a0d750aea11bb5cad769114
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-02-01 21:20:22 +0530

    Delete mainClass.java

--- Inheritance/multilevel_inheritance/mainClass.java
+++
@@ -1,9 +0,0 @@
-public class first_proj{
-    public static void main(String args[]){
-        AddSubMul n1 = new AddSubMul();
-        n1.setValue(30,20);
-        n1.sum();
-        n1.Sub();
-        n1.mul();
-    }
-}
