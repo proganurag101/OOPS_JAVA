746e0c6a4ca839387b1da44555d0b7a0a7c9f2b5
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-02-01 21:20:51 +0530

    Create Add.java

---
+++ Inheritance/multilevel_inheritance/Add.java
@@ -0,0 +1,11 @@
+public class Add {
+
+    int num1,num2;
+    public void setValue(int num1,int num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    public void sum(){
+        System.out.println("Sum: "+(num1+num2));
+    }
+}
