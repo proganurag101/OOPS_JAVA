4f5c8b19ebcd6a115d466373558db6a43c8cf722
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-02-01 21:19:36 +0530

    Create mainClass.java

---
+++ Inheritance/multilevel_inheritance/mainClass.java
@@ -0,0 +1,9 @@
+public class first_proj{
+    public static void main(String args[]){
+        AddSubMul n1 = new AddSubMul();
+        n1.setValue(30,20);
+        n1.sum();
+        n1.Sub();
+        n1.mul();
+    }
+}
