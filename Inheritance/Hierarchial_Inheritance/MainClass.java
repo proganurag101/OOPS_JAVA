d93fcec23db93883439a10f017a38b9660a45328
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-02-01 20:40:43 +0530

    Create MainClass.java

---
+++ Inheritance/Hierarchial_Inheritance/MainClass.java
@@ -0,0 +1,16 @@
+
+
+public class first_proj {
+    
+
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle();
+        t1.setBaseAndHeight(20.5,10);
+        t1.calcarea();
+
+        Rectangle r1 = new Rectangle();
+        r1.setBaseAndHeight(10,10);
+        r1.calcarea();
+
+   }
+}
