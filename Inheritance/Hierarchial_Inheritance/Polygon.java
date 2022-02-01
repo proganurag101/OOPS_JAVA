a707a84131e329c57ed60f7c47aaeab378668494
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-02-01 20:41:18 +0530

    Create Polygon.java

---
+++ Inheritance/Hierarchial_Inheritance/Polygon.java
@@ -0,0 +1,9 @@
+public class Polygon {
+    double base;
+    double height;
+    public void setBaseAndHeight(double base,double height){
+        this.base = base;
+        this.height = height;
+    }
+    
+}
