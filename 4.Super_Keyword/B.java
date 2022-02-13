2bfb1e541949bb8ba9f94a20e9eb09397e59d12b
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-02-13 14:18:37 +0530

    Create B.java

---
+++ 4.Super_Keyword/B.java
@@ -0,0 +1,12 @@
+public class B extends A {
+    int num1 = 200;
+    public void doThis(){
+        System.out.println("Do this from B!");
+
+    }
+    public void xyz(){
+        System.out.println(super.num1);
+       super.doThis();
+        System.out.println("XYZ executed!");
+    }
+}
