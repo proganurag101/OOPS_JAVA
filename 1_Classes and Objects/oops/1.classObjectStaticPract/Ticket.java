9630dec507bc870494132a889896a4cf25f38ebf
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-03-17 19:14:25 +0530

    Create Ticket.java

---
+++ 1_Classes and Objects/oops/1.classObjectStaticPract/Ticket.java
@@ -0,0 +1,51 @@
+public class Ticket{
+    private int ticketid;
+    private int price;
+    private static int availableTickets;
+    int booking;
+    
+    //setter_ticked_id
+    public void setTicketid(int id){
+        ticketid = id;
+    }
+    public int getTicketid(){
+        return this.ticketid;
+    }
+    
+    //getter_price
+    public int getPrice(){
+        return this.price;
+    }
+    //setter_price
+    public void setPrice(int val){
+        
+        this.price = val;
+        
+    }
+    
+    //getter_availableTickets
+    public static int getAvailableTickets(){
+        return availableTickets;
+    }
+    //setter_availableTickets
+    public static void setAvailableTickets(int num){
+        if(num>0){
+          availableTickets = num;
+        }
+    }
+    
+    public int calculateTicketCost(int nooftickets){
+        if(nooftickets<=getAvailableTickets()){
+            System.out.println("Available tickets: "+getAvailableTickets());
+            availableTickets -= nooftickets;
+            return nooftickets*getPrice();
+        }else{
+            return -1;
+        }
+    }
+    
+    
+    
+    
+    
+}
