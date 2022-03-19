6882802a6a59436c84bb60650db43fcfa2e73ee0
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-03-19 17:35:03 +0530

    Update StudentMain.java

--- 1_Classes and Objects/oops/2.Constructor_equals()/StudentMain.java
+++ 1_Classes and Objects/oops/2.Constructor_equals()/StudentMain.java
@@ -6,6 +6,8 @@ public class StudentMain{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Student's Id:");
         int id = sc.nextInt();
+        
+//1.        to reset the scanner cursor from int value.
         sc.nextLine();
         System.out.println("Enter Student's Name:");
         String name = sc.nextLine();
@@ -15,23 +17,31 @@ public class StudentMain{
         
         boolean isFrom=false;
         String Input = "";
+        
+        
+        
+        
+  //2.       Beautiful use of infinite loop
         while(true){
              
              System.out.println("Whether the student is from NIT(Yes/No):");
              Input = sc.nextLine();
+            
+  //3.       We don't use "==" as it is used for location comparison  but we use .equals() to compare the content in the string.
              if(Input.equals("Yes") || Input.equals("YES")){
                  isFrom=true;
                  break;
-             }else if(Input.equals("No") || Input.equals("NO")){
+             }else if(Input.equals("No") || Input.equals("NO")){ 
                  isFrom=false;
                  break;
              }else{
                  System.out.println("Wrong Input");
              }
         }
+    //4. Declare object globally
         Student s1;
        
-        if(isFrom){
+        if(isFrom){     //if(true)
              s1 = new Student(id,name,address);
         }else{
             System.out.println("Enter the college name:");
