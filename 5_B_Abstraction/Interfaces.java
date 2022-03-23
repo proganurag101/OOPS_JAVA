e1391d91a2d2cccc33c4c0139de1d12739596f86
proganurag101 <54454820+proganurag101@users.noreply.github.com>
2022-03-23 16:42:21 +0530

    Create Interfaces.java

---
+++ 5_B_Abstraction/Interfaces.java
@@ -0,0 +1,22 @@
+interface Animal{
+    int eyes =2;  //will be final(fixed value),static(same for all animals),[public]
+    void walk();  //by default public and abstract
+}
+
+class horse implements Animal{
+    public void walk(){  //you have to implement and write public
+        System.out.println("I walk on four legs");
+    }
+}
+
+
+
+
+public class main_oops{
+
+    public static void main(String[] args) {
+        horse h =  new horse();
+        h.walk();
+
+    }
+}
